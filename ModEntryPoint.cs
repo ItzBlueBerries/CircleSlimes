8871392db69555fe6c1662551d49f60178673200
Fruitsy <80174970+ItzBlueBerries@users.noreply.github.com>
2022-09-04 07:49:30 -0400

    Add project files.

---
+++ ModEntryPoint.cs
@@ -0,0 +1,119 @@
+﻿using HarmonyLib;
+using SRML;
+using SRML.SR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using static ShortcutLib.Shortcut;
+
+namespace CircleSlimes
+{
+    [HarmonyPatch(typeof(SRModLoader), "PostLoadMods")]
+    internal static class PostLoadMod
+    {
+        internal static AssetBundle circle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(typeof(Main), "circlemod"));
+        public static void Postfix()
+        {
+            foreach (Identifiable.Id id in Identifiable.SLIME_CLASS)
+            {
+                SlimeAppearance.SlimeBone[] attachedBones = new SlimeAppearance.SlimeBone[]
+                {
+                    SlimeAppearance.SlimeBone.JiggleBack,
+                    SlimeAppearance.SlimeBone.JiggleBottom,
+                    SlimeAppearance.SlimeBone.JiggleFront,
+                    SlimeAppearance.SlimeBone.JiggleLeft,
+                    SlimeAppearance.SlimeBone.JiggleRight,
+                    SlimeAppearance.SlimeBone.JiggleTop
+                };
+
+                if (Prefab.GetPrefab(id) != null && Slime.GetSlimeDef(id).AppearancesDefault[0] != null)
+                {
+                    (GameObject, SlimeAppearanceObject, SlimeAppearance.SlimeBone[]) circleBody = Structure.CreateBasicStructure(circle, "circle", "slime_circle_body", SlimeAppearance.SlimeBone.Slime, SlimeAppearance.SlimeBone.None, attachedBones, RubberBoneEffect.RubberType.Slime, true);
+                    circleBody.Item2.IgnoreLODIndex = true;
+                    AssetsLib.MeshUtils.GenerateBoneData(Prefab.GetPrefab(id).GetComponent<SlimeAppearanceApplicator>(), circleBody.Item2, 0.15f);
+                    Structure.SetStructureElement(Slime.GetSlimeDef(id).AppearancesDefault[0], new SlimeAppearanceObject[] { circleBody.Item2 }, 0, false, true);
+                }
+            }
+
+            foreach (Identifiable.Id id in Identifiable.LARGO_CLASS)
+            {
+                SlimeAppearance.SlimeBone[] attachedBones = new SlimeAppearance.SlimeBone[]
+                {
+                    SlimeAppearance.SlimeBone.JiggleBack,
+                    SlimeAppearance.SlimeBone.JiggleBottom,
+                    SlimeAppearance.SlimeBone.JiggleFront,
+                    SlimeAppearance.SlimeBone.JiggleLeft,
+                    SlimeAppearance.SlimeBone.JiggleRight,
+                    SlimeAppearance.SlimeBone.JiggleTop
+                };
+
+                if (Prefab.GetPrefab(id) != null && Slime.GetSlimeDef(id).AppearancesDefault[0] != null)
+                {
+                    (GameObject, SlimeAppearanceObject, SlimeAppearance.SlimeBone[]) circleBody = Structure.CreateBasicStructure(circle, "circle", "slime_circle_body", SlimeAppearance.SlimeBone.Slime, SlimeAppearance.SlimeBone.None, attachedBones, RubberBoneEffect.RubberType.Slime, true);
+                    circleBody.Item2.IgnoreLODIndex = true;
+                    AssetsLib.MeshUtils.GenerateBoneData(Prefab.GetPrefab(id).GetComponent<SlimeAppearanceApplicator>(), circleBody.Item2, 0.15f);
+                    Structure.SetStructureElement(Slime.GetSlimeDef(id).AppearancesDefault[0], new SlimeAppearanceObject[] { circleBody.Item2 }, 0, false, true);
+                }
+            }
+
+            foreach (Identifiable.Id id in Identifiable.PLORT_CLASS)
+            {
+                if (Prefab.GetPrefab(id) != null)
+                {
+                    Prefab.GetPrefab(id).GetComponent<MeshFilter>().sharedMesh = (Mesh)Other.LoadAsset(typeof(Mesh), circle, "circle_plort");
+                }
+            }
+
+            foreach (Identifiable.Id id in Identifiable.TOY_CLASS)
+            {
+                if (id == Identifiable.Id.ROBOT_TOY || id == Identifiable.Id.TREASURE_CHEST_TOY || id == Identifiable.Id.BOP_GOBLIN_TOY)
+                    continue;
+                if (Prefab.GetPrefab(id) != null)
+                {
+                    Prefab.GetPrefab(id).GetComponentInChildren<MeshFilter>().sharedMesh = (Mesh)Other.LoadAsset(typeof(Mesh), circle, "circle");
+                }
+            }
+
+            foreach (Identifiable.Id id in Identifiable.STANDARD_CRATE_CLASS)
+            {
+                if (Prefab.GetPrefab(id) != null)
+                    Prefab.GetPrefab(id).GetComponentInChildren<MeshFilter>().sharedMesh = (Mesh)Other.LoadAsset(typeof(Mesh), circle, "circle");
+            }
+
+            foreach (Identifiable.Id id in Identifiable.CRAFT_CLASS)
+            {
+                if (Prefab.GetPrefab(id) != null)
+                    Prefab.GetPrefab(id).GetComponentInChildren<MeshFilter>().sharedMesh = (Mesh)Other.LoadAsset(typeof(Mesh), circle, "circle");
+            }
+        }
+    }
+
+    public class Main : ModEntryPoint
+    {
+        // Called before GameContext.Awake
+        // You want to register new things and enum values here, as well as do all your harmony patching
+        public override void PreLoad()
+        {
+            HarmonyInstance.PatchAll();
+        }
+
+
+        // Called before GameContext.Start
+        // Used for registering things that require a loaded gamecontext
+        public override void Load()
+        {
+
+        }
+
+        // Called after all mods Load's have been called
+        // Used for editing existing assets in the game, not a registry step
+        public override void PostLoad()
+        {
+
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CircleSlimes")]
+[assembly: AssemblyDescription("Transforms all slimes into a circular body, is that entertaining enough or do you need triangle slimes? Stop morphing their bodies.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CircleSlimes")]
+[assembly: AssemblyCopyright("Copyright © FruitsyOG 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("690b9a0a-4152-4d3a-bc4b-b3184c28fcb2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
